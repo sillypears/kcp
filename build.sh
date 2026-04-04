8c4853818087f632eea80b78a0edbe8ac667f855
sillypears <sillypears@users.noreply.github.com>
2026-04-04 12:55:22 -0400

    Build and DEPLOY

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-cd frontend && npm run build && cd ..
-time docker buildx build -t kc_purchases:latest --platform linux/amd64 .
+cd frontend && npm run build && cd .. &&
+time docker buildx build -t kc_purchases:latest --platform linux/amd64 . &&
+docker-compose down && docker-compose up -d
